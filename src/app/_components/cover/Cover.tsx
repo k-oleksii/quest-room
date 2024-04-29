729cde8f9e15b07aa94f2c1cfd785164a95184c9
Aleksey Kozyuberda <kozyuberda.aleksey@gmail.com>
2024-04-30 02:05:25 +0300

    Added page for quest

--- src/app/_components/cover/Cover.tsx
+++ src/app/_components/cover/Cover.tsx
@@ -1,17 +1,26 @@
 import Image from 'next/image';
 import { FC } from 'react';
 
-const Cover: FC = () => {
+interface ICover {
+  alt: string;
+  src: string;
+  inner?: boolean;
+}
+
+const Cover: FC<ICover> = props => {
   return (
-    <div className="absolute inset-0 overflow-hidden after:absolute after:inset-0 after:bg-cover-pattern">
+    <div
+      className={`absolute inset-0 overflow-hidden after:absolute after:inset-0 ${
+        props.inner ? 'after:bg-cover-inner-pattern' : 'after:bg-cover-pattern'
+      }`}
+    >
       <Image
-        alt="Cover"
-        src="/main-bg.jpg"
-        width={1428}
-        height={824}
+        alt={props.alt}
+        src={props.src}
+        fill
         sizes="100vw"
         quality={80}
-        className="w-full h-full object-cover object-center blur-xl"
+        className={`object-cover ${props.inner ? 'blur-sm	' : 'blur-xl'}`}
       />
     </div>
   );
